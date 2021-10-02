6cf766dc8a218658d06c5b86823293a55b373bae
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-02 21:49:48 +0000

    es27pag178

---
+++ src/es27pag177.java
@@ -0,0 +1,14 @@
+public class es27pag177 {
+    public static void main(String[] args){
+        //inserire il prezzo del primo libro
+        int libro1;
+        //inserire il prezzo del secondo libro
+        int libro2;
+        //inserire il prezzo del  terzo libro
+        int libro3;
+        //inserire il prezzo del quarto libro
+        int libro4;
+        //calcolare il costo medio di un libro
+        System.out.println((libro1+libro2+libro3+libro4)/4);
+    }
+}
